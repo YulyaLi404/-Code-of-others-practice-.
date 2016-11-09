07fa92b796ce1421201c5869173ba04a57166f7d
Crow404 <YulyaLi404@gmail.com>
2016-11-09 09:41:20 +0300

    Update Search.java

--- src/Search.java
+++ src/Search.java
@@ -1,16 +1,18 @@
 package Search;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by Crow on 08.11.2016.
- * Считывает символы с клавиатуры, и выводит их, пока не встретится точка.Все делается в цикле do-while.
  */
 
 public class Search {
 
     public static void main(String[] args) throws IOException {
 
+        // Считывает символы с клавиатуры, и выводит их, пока не встретится точка.Все делается в цикле do-while.
         System.out.println("Введите символы: ");
 
         char s, ignore;
@@ -26,5 +28,26 @@ public class Search {
             //после второго символа и найдя его идет далее по коду операторов, а
             //переменная ch присвоила изначально первый символ!
         }while (s != '.');
+        
+        
+        // Напишите программу, которая считывает символы пока не встретится точка. 
+        // Также предусмотрите вывод количества пробелов.
+        
+        BufferedReader x = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Введите строку: ");
+
+        String input = x.readLine();
+        String output = input.substring(0, input.indexOf('.'));
+
+        System.out.println( output );
+
+        String s = " ";
+        String a = output;
+        int l = a.length();
+        a = a.replaceAll(s, "");
+        int q = (l - a.length()) / s.length();
+
+        System.out.println("количество пробелов: " + q);
     }
 }
